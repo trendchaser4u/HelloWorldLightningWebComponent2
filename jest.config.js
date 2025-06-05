ab5230d1e9a73014b0ccdb53beb3ee560644f1db
Boobalan Muthukumaran <boobalanmaker@gmail.com>
2025-06-05 11:27:55 +0530

    Update jest.config.js to include 'changed-sources' in modulePathIgnorePatterns and coverage exclusions

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,14 @@ const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 
 module.exports = {
   ...jestConfig,
-  modulePathIgnorePatterns: ["<rootDir>/.localdevserver"],
+  modulePathIgnorePatterns: ["<rootDir>/.localdevserver", "<rootDir>/changed-sources"],
   coverageDirectory: "coverage",
   collectCoverage: true,
   collectCoverageFrom: [
     "force-app/main/default/lwc/**/*.js",
     "!**/node_modules/**",
     "!**/__tests__/**",
-    "!**/staticresources/**"
+    "!**/staticresources/**",
+    "!**/changed-sources/**"
   ]
 };
